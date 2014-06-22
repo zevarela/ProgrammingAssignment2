fec7139cb83c9bea2e984bc779e979cf0189f4d2
zevarela <zav@ZavMbp.local>
2014-06-22 22:39:29 +0100

    Programing Assignment 2
    My solution to the second programming assignment in R Programing
    Course, from John Hopkins BSoPH / Coursera

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is an example of R code to preserve state in a different environment, that can be used for caching data.
+## The functions bellow will create a "matrix extension" that can cache it's inverse, and a sover function
+## that uses the cached version if available. 
+## It was my first attempt at this assigment, and my first use of GitHub! Thanks for checking it out.
 
-## Write a short comment describing this function
 
+## This function was created as my first attempt at an R extension of the matrix object that caches it's inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+  inverse <- NULL
+  set <- function(new.matrix) {
+    if (new.matrix!=x) # only update the matrix when needed
+    {
+      x <<- new.matrix
+      inverse <<- NULL
+    }
+  }
+  get <- function() x
+  set.inverse <- function(solved.inverse) inverse <<- solved.inverse
+  get.inverse <- function() inverse
+  list(set = set, get = get, set.inverse = set.inverse, get.inverse = get.inverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function receives a makeCacheMatrix object and returns the inverse of it's matrix.
+## returns the cached inverse it the matrix has already been inverted before
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$get.inverse()
+  if(!is.null(inv)) {
+    message("Inverse was previously computed, returning cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$set.inverse(inv)
+  inv
 }
